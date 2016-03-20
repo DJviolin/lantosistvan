f4bdb75e83b43e40f8361549cb142ff6dcf904e8
DJviolin <kerozin.joe@gmail.com>
2016-03-20 19:18:34 +0100

    refact

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -59,9 +59,9 @@ $(document).ready(function() {
 // Latest posts resize on frontpage
 $(document).ready(function() {
   $(window).on('load resize', function() {
-  var flpImgWidth = $('.frontpage-latest-posts article img').width();
+  var flpImgWidth = $('.latest-posts article img').width();
   var calc = ( flpImgWidth / 4 ) * 3;
-  $('.frontpage-latest-posts article img').css('height', calc);
+  $('.latest-posts article img').css('height', calc);
   //console.log(calc);
   });
 });
