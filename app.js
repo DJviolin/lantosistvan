93760241ccde28f651d8e2603e37734ef08090e8
DJviolin <kerozin.joe@gmail.com>
2016-03-22 22:21:33 +0100

    css work

--- app.js
+++ app.js
@@ -75,10 +75,12 @@ app.all('*?lang=en', function (req, res) {
 app.all('/valami', function (req, res) {
   if(req.query.lang === 'hu') {
     res.cookie('locale', 'hu', { maxAge: 900000, httpOnly: true })
+    res.send('CookiesHU: ', req.cookies);
     console.log('CookiesHU: ', req.cookies);
   };
   if(req.query.lang === 'en') {
     res.cookie('locale', 'en', { maxAge: 900000, httpOnly: true })
+    res.send('CookiesEN: ', req.cookies);
     console.log('CookiesEN: ', req.cookies);
   };
 });
