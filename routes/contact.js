a62191a3f4165282d1d859280b0b8040875ed3b9
DJviolin <kerozin.joe@gmail.com>
2016-03-29 13:23:14 +0200

    contact.js async work

--- routes/contact.js
+++ routes/contact.js
@@ -4,13 +4,29 @@ var express = require('express'),
     router  = express.Router();
 
 /* GET info page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('contact', {
     titleShown: true,
     title: 'Contact',
     description: 'Get in touch',
     keywords: 'info,wedding,photography,film,lantos,istvan'
   });
+});*/
+
+/* GET info page. */
+/* Async Test */
+router.get('/', (req, res, next) => {
+  res.render('contact', {
+    titleShown: true,
+    title: 'Contact',
+    description: 'Get in touch',
+    keywords: 'info,wedding,photography,film,lantos,istvan'
+  }, (err, html) => {
+    if (err) throw err;
+    res.send(html);
+    console.log('sent this static stuff');
+  });
+  console.log('this is async');
 });
 
 module.exports = router;
