6f7b8eb1074422ede2df30204e02b48889c3919c
DJviolin <kerozin.joe@gmail.com>
2016-03-29 20:51:24 +0200

    css work

--- routes/contact.js
+++ routes/contact.js
@@ -3,13 +3,30 @@
 var express = require('express'),
     router  = express.Router();
 
+var functions  = require('../lib/functions'),
+    fsAsync    = functions.fsAsync;
+
 /* GET info page. */
 router.get('/', function(req, res, next) {
-  res.render('contact', {
-    titleShown: true,
-    title: 'Contact',
-    description: 'Get in touch',
-    keywords: 'info,wedding,photography,film,lantos,istvan'
+  fsAsync(function (err, data) {
+    if (err) {
+      return res.render('404', {
+        titleShown: true,
+        title: 'Error 404',
+        description: 'Error 404',
+        keywords: 'error,404'
+      });
+    }
+
+    var contact = data[2].contact;
+
+    res.render('contact', {
+      titleShown: true,
+      title: 'Contact',
+      description: 'Get in touch',
+      keywords: 'info,wedding,photography,film,lantos,istvan',
+      data: contact
+    });
   });
 });
 
