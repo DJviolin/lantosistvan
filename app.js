ccd115e72c60223de73325aa0309639faf1007a9
DJviolin <kerozin.joe@gmail.com>
2016-04-11 22:49:20 +0200

    routes refact

--- app.js
+++ app.js
@@ -150,16 +150,25 @@ var langRouter = function(req, res, next) {
   res.locals.language = '/' + selectedLang;
   next();
 };
-app.all('/:lang/*', langRouter);
-app.use('/:lang', langRouter);
-
-// Add i18n CSS class to <html> tag
-app.use(function(req, res, next) {
+var langClass = function(req, res, next) {
   var defaultLang = 'hu';
-  var activeLang = req.cookies.locale || defaultLang;
+  //var activeLang = req.cookies.locale || defaultLang;
+  var activeLang = req.params.lang || defaultLang;
   res.locals.langClass = activeLang + '-' + activeLang.toUpperCase(); // views/layout-top.hbs
   next();
-});
+};
+app.all('*', langClass); // Making sure function is executed without any query.param
+app.all('/:lang/*', langRouter, langClass);
+app.use('/:lang', langRouter, langClass);
+
+// Add i18n CSS class to <html> tag
+/*app.use(function(req, res, next) {
+  var defaultLang = 'hu';
+  //var activeLang = req.cookies.locale || defaultLang;
+  var activeLang = req.params.lang || defaultLang;
+  res.locals.langClass = activeLang + '-' + activeLang.toUpperCase(); // views/layout-top.hbs
+  next();
+});*/
 
 /////////////////////////////////////////////////////////////
 // INIT ROUTES
