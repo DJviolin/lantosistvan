3573390386ea4fe5b7f418b3fbaa118673b6b6d6
DJviolin <kerozin.joe@gmail.com>
2016-04-11 23:14:34 +0200

    routes refact

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ app.use('/:lang/contact', contact);
 // Place under every other routes, because it can block others!
 //app.use('/', index);
 app.use('/', function (req, res) {
-  res.redirect('/' + langClass);
+  res.redirect('/' + req.getLocale());
 });
 app.use('/:lang', index);
 
