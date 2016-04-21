ab18a8e394b84fd17ad1ac2be3ef76f8a0a37bde
DJviolin <kerozin.joe@gmail.com>
2016-04-21 20:42:42 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@
 /////////////////////////////////////////////////////////////
 
 const gulp       = require('gulp'),
-      shell      = require('gulp-shell'),
+      //shell      = require('gulp-shell'),
       cleanCSS   = require('gulp-clean-css'),
       rename     = require('gulp-rename'),
-      livereload = require('gulp-livereload'),
+      //livereload = require('gulp-livereload'),
       server     = require('gulp-express'),
       uglify     = require('gulp-uglify');
 
 const paths = {
   pathCleanCSS:  ['public/stylesheets/style.css'],
-  pathServer:    ['app.js', 'config.js', 'routes/*.js', 'lib/*.js', 'bin/www'],
+  pathServer:    ['app.js', 'bin/www', 'routes/**/*.js', 'lib/**/*.js', 'config.js'],
   pathJsUglify:  ['public/javascripts/main-vanilla.js', 'public/javascripts/main-jquery.js'],
   pathFiles:     ['views/*.hbs', 'views/**/*.hbs', 'database/data.json', 'locales/*.json']
 };
@@ -28,11 +28,11 @@ const paths = {
 ]));*/
 
 gulp.task('start', function () {
-  return gulp.src(paths.pathServer)
+  /*return gulp.src(paths.pathServer)
     .pipe(shell([
       'node --trace-deprecation --trace-sync-io ./bin/www'
-    ]));
-  //server.run(['node --trace-deprecation --trace-sync-io ./bin/www']);
+    ]));*/
+  server.run(['bin/www']);
 })
 
 /////////////////////////////////////////////////////////////
@@ -45,8 +45,8 @@ gulp.task('minify-css', function() {
       console.log(details.name + ': The file was reduced from ' + details.stats.originalSize + ' bytes to ' + details.stats.minifiedSize + ' bytes. This means ' + Math.round(details.stats.efficiency * 100) + '% reduction in size!');
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('public/stylesheets'))
-    .pipe(livereload());
+    .pipe(gulp.dest('public/stylesheets'));
+    //.pipe(livereload());
 });
 
 /////////////////////////////////////////////////////////////
@@ -57,8 +57,8 @@ gulp.task('uglify', function() {
   return gulp.src(paths.pathJsUglify)
     .pipe(uglify({ output: {quote_style: 1} }))
     .pipe(rename({ extname: '.min.js' }))
-    .pipe(gulp.dest('public/javascripts'))
-    .pipe(livereload());
+    .pipe(gulp.dest('public/javascripts'));
+    //.pipe(livereload());
 });
 
 /////////////////////////////////////////////////////////////
@@ -66,20 +66,35 @@ gulp.task('uglify', function() {
 /////////////////////////////////////////////////////////////
 
 gulp.task('files', function() {
-  return gulp.src(paths.pathFiles)
-    .pipe(livereload());
+  return gulp.src(paths.pathFiles);
+    //.pipe(livereload());
 });
 
 /////////////////////////////////////////////////////////////
 // WATCH
 /////////////////////////////////////////////////////////////
 
-gulp.task('watch', function() { // Rerun the task when a file changes
+/*gulp.task('watch', function() { // Rerun the task when a file changes
   livereload.listen();
-  gulp.watch(paths.pathServer, ['start']).on('change', livereload.changed);
+  //gulp.watch(paths.pathServer, ['start']).on('change', livereload.changed);
+  //gulp.watch(paths.pathServer, ['start']).on('change', server.run);
+  gulp.watch(paths.pathServer, ['start']).on('change', server.notify);
   gulp.watch(paths.pathCleanCSS, ['minify-css']).on('change', livereload.changed);
   gulp.watch(paths.pathJsUglify, ['uglify']).on('change', livereload.changed);
   gulp.watch(paths.pathFiles, ['files']).on('change', livereload.changed);
+
+  //gulp.watch([paths.pathServer, 'routes/*.js'], [server.run]);
+});*/
+
+gulp.task('watch', function() { // Rerun the task when a file changes
+  //livereload.listen();
+  //gulp.watch(paths.pathServer, ['start']).on('change', livereload.changed);
+  gulp.watch(paths.pathServer, ['start']).on('change', server.notify);
+  gulp.watch(paths.pathCleanCSS, ['minify-css']).on('change', server.notify);
+  gulp.watch(paths.pathJsUglify, ['uglify']).on('change', server.notify);
+  gulp.watch(paths.pathFiles, ['files']).on('change', server.notify);
+
+  //gulp.watch([paths.pathServer, 'routes/**/*.js'], [server.run]);
 });
 
 /////////////////////////////////////////////////////////////
@@ -87,25 +102,24 @@ gulp.task('watch', function() { // Rerun the task when a file changes
 /////////////////////////////////////////////////////////////
 
 gulp.task('server', function () {
-    // Start the server at the beginning of the task 
-    server.run(['app.js']);
- 
-    // Restart the server when file changes 
-    gulp.watch(['app/**/*.html'], server.notify);
-    gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
-    //gulp.watch(['{.tmp,app}/styles/**/*.css'], ['styles:css', server.notify]); 
-    //Event object won't pass down to gulp.watch's callback if there's more than one of them. 
-    //So the correct way to use server.notify is as following: 
-    gulp.watch(['{.tmp,app}/styles/**/*.css'], function(event){
-        gulp.run('styles:css');
-        server.notify(event);
-        //pipe support is added for server.notify since v0.1.5, 
-        //see https://github.com/gimm/gulp-express#servernotifyevent 
-    });
- 
-    gulp.watch(['app/scripts/**/*.js'], ['jshint']);
-    gulp.watch(['app/images/**/*'], server.notify);
-    gulp.watch(['app.js', 'routes/**/*.js'], [server.run]);
+  // Start the server at the beginning of the task 
+  server.run(['bin/www']);
+
+  // Restart the server when file changes 
+  gulp.watch([paths.pathFiles], server.notify);
+  gulp.watch([paths.pathCleanCSS], ['styles:css', server.notify]); 
+  //Event object won't pass down to gulp.watch's callback if there's more than one of them. 
+  //So the correct way to use server.notify is as following: 
+  gulp.watch(['{.tmp,app}/styles/**/*.css'], function(event){
+    gulp.run('styles:css');
+    server.notify(event);
+    //pipe support is added for server.notify since v0.1.5, 
+    //see https://github.com/gimm/gulp-express#servernotifyevent 
+  });
+
+  gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+  gulp.watch(['app/images/**/*'], server.notify);
+  gulp.watch(['app.js', 'routes/**/*.js'], [server.run]);
 });
 
 /////////////////////////////////////////////////////////////
@@ -113,7 +127,7 @@ gulp.task('server', function () {
 /////////////////////////////////////////////////////////////
 
 gulp.task('default', ['start', 'minify-css', 'uglify', 'files', 'watch']);
-
+//gulp.task('default', ['server']);
 
 
 
