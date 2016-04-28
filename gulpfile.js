bd6d63c0fe6032d3f4f34ce9608e5c25d8be40b5
DJviolin <kerozin.joe@gmail.com>
2016-04-28 14:57:58 +0200

    refact

--- gulpfile.js
+++ gulpfile.js
@@ -25,9 +25,7 @@ const paths = {
 
 // this will achieve `node --trace-deprecation --trace-sync-io ./bin/www`
 const server = gls.new([
-  '--trace-deprecation',
-  '--trace-sync-io',
-  'bin/www',
+  '--trace-deprecation', '--trace-sync-io', 'bin/www',
   { env: { NODE_ENV: 'production' } }
 ]);
 
