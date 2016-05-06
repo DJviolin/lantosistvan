fb329a2a2f240cab375de12638ab636fa9a57e03
DJviolin <kerozin.joe@gmail.com>
2016-05-06 13:36:57 +0200

    refact

--- config/routes.js
+++ config/routes.js
@@ -1,22 +1,5 @@
 'use strict';
 
-// PORT
-/*const usedPort = '3000';
-exports.usedPort = usedPort;*/
-
-/*// API TOKEN
-const apiToken = 'password';
-exports.apiToken = apiToken;
-
-// JSON DATABASE
-// PATH RELATIVE TO: ./lib/functions.js
-const db = '/../database/data.json';
-exports.db = db;
-
-// POSTS PER PAGE
-const articlesPerPage = 2;
-exports.articlesPerPage = articlesPerPage;*/
-
 module.exports = {
   apiToken: 'password',
   db: '/../database/data.json',
