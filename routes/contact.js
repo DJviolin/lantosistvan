8b3bf38586becdd0ff956b6244d9d1279e0a8541
DJviolin <kerozin.joe@gmail.com>
2016-05-08 22:02:59 +0200

    refact

--- routes/contact.js
+++ routes/contact.js
@@ -50,7 +50,15 @@ router.post('/', (req, res) => {
 
   // You have to check the captcha before you call sendMail.
   // There is no way to abort already running email transaction in Nodemailer
-  if(req.body.captcha === 'négy' || 'negy' || 'Négy' || 'Negy' || 'NÉGY' || 'NEGY' || 'four' || 'Four' || 'FOUR') {
+  if(req.body.captcha === 'négy' ||
+     req.body.captcha === 'negy' ||
+     req.body.captcha === 'Négy' ||
+     req.body.captcha === 'Negy' ||
+     req.body.captcha === 'NÉGY' ||
+     req.body.captcha === 'NEGY' ||
+     req.body.captcha === 'four' ||
+     req.body.captcha === 'Four' ||
+     req.body.captcha === 'FOUR') {
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (err, info) => {
       if(err) {
