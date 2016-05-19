42449ae2e3fd0cbe3279b5b9dfc5c2237cea1ded
DJviolin <kerozin.joe@gmail.com>
2016-05-19 11:45:50 +0200

    ajax test

--- __misc/NOTES.md
+++ __misc/NOTES.md
@@ -1,3 +1,7 @@
+# AJAX
+
+http://mherman.org/blog/2013/10/20/handling-ajax-calls-with-node-dot-js-and-express-scraping-craigslist/
+
 # package.json uncommented
 
 ```
--- public/javascripts/src/main-vanilla.js
+++ public/javascripts/src/main-vanilla.js
@@ -179,14 +179,21 @@ email.addEventListener('keyup', function() {
 
 //(function() {
   function ajax() {
-    var xhttp = new XMLHttpRequest();
+    var xhttp;
+    if (window.XMLHttpRequest) {
+      // code for modern browsers
+      xhttp = new XMLHttpRequest();
+    } else {
+      // code for IE6, IE5
+      xhttp = new ActiveXObject('Microsoft.XMLHTTP');
+    };
     xhttp.onreadystatechange = function() {
       if (xhttp.readyState === 4 && xhttp.status === 200) {
-       document.getElementById('demo').innerHTML = xhttp.responseText;
-      }
+        document.getElementById('demo').innerHTML = xhttp.responseText;
+      };
     };
     xhttp.open('GET', '/ajax_info.txt', true);
     xhttp.send();
-  };
+  }
 //  ajax();
 //})();
