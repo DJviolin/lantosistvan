7ce1669f7eda9661a0d79f6ea87e7c3a5d24a79d
DJviolin <kerozin.joe@gmail.com>
2016-05-22 16:58:17 +0200

    refact

--- routes/contact.js
+++ routes/contact.js
@@ -100,130 +100,8 @@ router.use((req, res, next) => {
   next();
 });
 
-router.use((req, res, next) => {
-  // Variables
-  /*const firstname = req.body.firstname,
-        surname   = req.body.surname,
-        email     = req.body.email,
-        subject   = req.body.subject,
-        message   = req.body.message,
-        captcha   = req.body.captcha;*/
-  //firstname = null;
-  //captcha   = null;
-  /*console.log(req.firstname + '\n\n' +
-              req.surname + '\n\n' +
-              req.email + '\n\n' +
-              req.subject + '\n\n' +
-              req.message + '\n\n' +
-              req.captcha);
-  // create reusable transporter object using the default SMTP transport
-  req.transporter = nodemailer.createTransport({
-    pool: false,
-    host: config.host,
-    port: config.port,
-    secure: true, // use SSL
-    auth: {
-      user: config.user,
-      pass: config.pass
-    },
-    logger: true, // log to console
-    debug: true, // include SMTP traffic in the logs
-    // use up to 5 parallel connections
-    maxConnections: 5,
-    // do not send more than 10 messages per connection
-    maxMessages: 10,
-    // do not send more than 5 messages in a second
-    rateLimit: 5
-  });
-  // setup e-mail data with unicode symbols
-  req.mailOptions = {
-    from: '"👥 LantosIstvan.com" <' + config.from + '>', // sender address
-    to: config.to, // list of receivers
-    subject: '<< Contact Form >>', // Subject line
-    text: // plaintext body
-      'Captcha:\n' + '    ' + req.body.captcha + '\n\n' +
-      'Nyelv:\n' + '    ' + req.getLocale() + '\n\n' +
-      'Keresztnév:\n' + '    ' + req.body.firstname + '\n\n' +
-      'Vezetéknév:\n' + '    ' + req.body.surname + '\n\n' +
-      'Email cím:\n' + '    ' + req.body.email + '\n\n' +
-      'Tárgy:\n' + '    ' + req.body.subject + '\n\n' +
-      'Üzenet:\n' + '    ' + req.body.message
-  };*/
-
-  //res.set('Content-Type', 'text/html');
-  //res.type('html');
-  //console.log('Content-Type (router.use): ' + req.get('Content-Type'));
-
-  // http://stackoverflow.com/questions/37368357/express-why-cannot-use-req-body-value-in-router-use#comment62249585_37368357
-  /*if(req.firstname.length === 0 ||
-     !req.firstname.match(/\D+/igm)) {
-    console.log('AJAX ERROR: Firstname is empty and/or have a number. Value: ' + req.firstname);
-    var validateFirstname = false;
-  } else {
-    console.log('AJAX OK: firstname. Value: ' + req.firstname);
-    var validateFirstname = true;
-  };
-
-  if(req.captcha.length === 0 ||
-    !req.captcha.match(/^kettő|ketto|two$/igm)) {
-    console.log('AJAX ERROR: captcha is empty and/or the entered value is invalid. Value: ' + req.captcha);
-    var validateCaptcha = false;
-  } else {
-    console.log('AJAX OK: captcha. Value: ' + captcha);
-    var validateCaptcha = true;
-  };
-
-  if(validateFirstname === true && validateCaptcha === true) {
-    console.log('SUCCESS: Form validated!');
-    // send mail with defined transport object
-    req.success;
-  } else {
-    console.log('ERROR: Form not validated!');
-    req.failure;
-  };*/
-
-  next();
-});
-
 /*router.use((req, res, next) => {
   console.log(req.body); // populated!
-  next();
-});*/
-
-/*router.all('*', (req, res, next) => {
-  // Variables
-  const firstname = req.body.firstname,
-        surname   = req.body.surname,
-        email     = req.body.email,
-        subject   = req.body.subject,
-        message   = req.body.message,
-        captcha   = req.body.captcha;
-  // http://stackoverflow.com/questions/37368357/express-why-cannot-use-req-body-value-in-router-use#comment62249585_37368357
-  if(firstname.length === 0 ||
-     !firstname.match(/\D+/igm)) {
-    console.log('AJAX ERROR: Firstname is empty and/or have a number. Value: ' + firstname);
-    var validateFirstname = false;
-  } else {
-    console.log('AJAX OK: firstname. Value: ' + firstname);
-    var validateFirstname = true;
-  };
-
-  if(captcha.length === 0 ||
-    !captcha.match(/^kettő|ketto|two$/igm)) {
-    console.log('AJAX ERROR: captcha is empty and/or the entered value is invalid. Value: ' + captcha);
-    var validateCaptcha = false;
-  } else {
-    console.log('AJAX OK: captcha. Value: ' + captcha);
-    var validateCaptcha = true;
-  };
-
-  if(validateFirstname === true && validateCaptcha === true) {
-    console.log('SUCCESS: Form validated!');
-    // send mail with defined transport object
-    req.success;
-  } else {
-    console.log('ERROR: Form not validated!');
-  };
   next();
 });*/
 
@@ -244,51 +122,7 @@ router.post('/', (req, res, next) => {
       console.log('Server responded with "%s"', info.response);
     });
   };
-
-  //console.log('Content-Type (router.post): ' + req.get('Content-Type'));
-
-  /*if(req.body.firstname.length === 0 ||
-     !req.body.firstname.match(/\D+/igm)) {
-    console.log('AJAX ERROR: Firstname is empty and/or have a number. Value: ' + req.body.firstname);
-    var validateFirstname = false;
-  }
-  else {
-    console.log('AJAX OK: firstname. Value: ' + req.body.firstname);
-    var validateFirstname = true;
-  };
-
-  if(req.body.captcha.length === 0 ||
-    !req.body.captcha.match(/^kettő|ketto|two$/igm)) {
-    console.log('AJAX ERROR: captcha is empty and/or the entered value is invalid. Value: ' + req.body.captcha);
-    var validateCaptcha = false;
-  }
-  else {
-    console.log('AJAX OK: captcha. Value: ' + req.body.captcha);
-    var validateCaptcha = true;
-  };
-
-  if(validateFirstname === true && validateCaptcha === true) {
-    console.log('SUCCESS: Form validated!');
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (err, info) => {
-      if(err) {
-        console.log('Error occurred');
-        console.log(err.message);
-      }
-      console.log('Message sent successfully!');
-      console.log('Server responded with "%s"', info.response);
-    });
-  } else {
-    console.log('ERROR: Form not validated!');
-    //res.send({ formValidationError: 'display: block;' });
-    //const formValidationError = true;
-    //res.locals.formValidationError = 'display: block;';
-    //next();
-    //res.locals.formValidationError = 'display: block;';
-    //res.send('contact', {formValidationError: 'display: block;'});
-  };*/
-
-  //res.status(302).redirect('/' + req.getLocale() + '/contact');
+  next();
 });
 
 /////////////////////////////////////////////////////////////
@@ -311,7 +145,10 @@ router.get('/', (req, res, next) => {
 
     //console.log('Content-Type (router.get): ' + req.get('Content-Type'));
 
-    if(req.sending === true) { var formValidationError = 'display: block;' };
+    if(req.sending === false) {
+      var formValidationError = 'display: block;'
+      return formValidationError;
+    };
 
     res.render('contact', {
       bodyClass: 'contact',
