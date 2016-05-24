6dc37b3b89559e5b8016726f042ce2f0552644ed
DJviolin <kerozin.joe@gmail.com>
2016-05-24 21:24:24 +0200

    refact

--- routes/users.js
+++ routes/users.js
@@ -3,26 +3,11 @@
 const util    = require('util'),
       express = require('express'),
       router  = express.Router();
-//const Client  = require('node-rest-client').Client;
-//const client  = new Client();
 const request = require('request');
 const parseString = require('xml2js').parseString;
 
 /////////////////////////////////////////////////////////////
 // EXTERNAL API REQUEST
-/////////////////////////////////////////////////////////////
-
-//const reqURL = 'http://www.eporner.com/api_xml/blowjob/5/10/adddate';
-
-/*request(reqURL, (error, response, body) => {
-  parseString(body, {trim: false}, (err, result) => {
-    //console.log([{ articles: result }]);
-    console.log(util.inspect(result, { showHidden: false, depth: null }));
-  });
-});*/
-
-/////////////////////////////////////////////////////////////
-// PAGE RENDERING
 /////////////////////////////////////////////////////////////
 
 router.get('/:tag/:start/:end/:order/', (req, res, next) => {
@@ -58,30 +43,6 @@ router.get('/:tag/:page', (req, res, next) => {
   });
 
 });
-
-/////////////////////////////////////////////////////////////
-// EXTERNAL API REQUEST
-/////////////////////////////////////////////////////////////
-
-/*router.get('/', (req, res, next) => {
-  const reqURL = req.protocol + '://' + req.hostname + ':' + usedPort + '/api/articles/page/' + 0 + '/' + articlesPerPage + '/order/' + 'adddate' + '/auth/' + apiToken;
-  request(reqURL, (error, response, body) => {
-    console.log([{ articles: JSON.parse(body) }]);
-    const articles = [{ articles: JSON.parse(body) }];
-    res.render('blog', {
-      bodyClass: 'Users',
-      active: { blog: true },
-      titleShown: true,
-      title: 'Users',
-      description: '',
-      keywords: '',
-      divClass: 'Users',
-      data: articles,
-      paginationParams: null,
-      paginationParamsSlash: false
-    });
-  })
-});*/
 
 /////////////////////////////////////////////////////////////
 // INIT EXPRESS ROUTER
