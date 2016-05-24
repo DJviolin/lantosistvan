5ad45d024bb1476e58e0515fe40fc9ca606cca3d
DJviolin <kerozin.joe@gmail.com>
2016-05-24 20:29:53 +0200

    refact

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,48 @@
 'use strict';
 
-const express = require('express'),
+const util    = require('util'),
+      express = require('express'),
       router  = express.Router();
+//const Client  = require('node-rest-client').Client;
+//const client  = new Client();
+const request = require('request');
+const parseString = require('xml2js').parseString;
 
 /////////////////////////////////////////////////////////////
-// GET users listing
+// EXTERNAL API REQUEST
 /////////////////////////////////////////////////////////////
 
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+const reqURL = 'http://www.eporner.com/api_xml/blowjob/5/10/adddate';
+request(reqURL, (error, response, body) => {
+  parseString(body, {trim: false}, (err, result) => {
+    //console.log([{ articles: result }]);
+    console.log(util.inspect(result, { showHidden: false, depth: null }));
+  });
 });
+
+/////////////////////////////////////////////////////////////
+// EXTERNAL API REQUEST
+/////////////////////////////////////////////////////////////
+
+/*router.get('/', (req, res, next) => {
+  const reqURL = req.protocol + '://' + req.hostname + ':' + usedPort + '/api/articles/page/' + 0 + '/' + articlesPerPage + '/order/' + 'adddate' + '/auth/' + apiToken;
+  request(reqURL, (error, response, body) => {
+    console.log([{ articles: JSON.parse(body) }]);
+    const articles = [{ articles: JSON.parse(body) }];
+    res.render('blog', {
+      bodyClass: 'Users',
+      active: { blog: true },
+      titleShown: true,
+      title: 'Users',
+      description: '',
+      keywords: '',
+      divClass: 'Users',
+      data: articles,
+      paginationParams: null,
+      paginationParamsSlash: false
+    });
+  })
+});*/
 
 /////////////////////////////////////////////////////////////
 // INIT EXPRESS ROUTER
