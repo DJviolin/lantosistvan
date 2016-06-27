70c23ec5e5737c7b72346c67a63975b601b43064
DJviolin <kerozin.joe@gmail.com>
2016-06-27 20:41:04 +0200

    go iris

--- go/go-get.sh
+++ go/go-get.sh
@@ -3,8 +3,12 @@
 # set -e making the commands if they were like &&
 set -e
 
-go get github.com/tools/godep
+gb vendor list
 
-echo -e "\nDownloading Go packages:\n"
-mkdir -p ./vendor/src/github.com/kataras/iris
-git clone https://github.com/kataras/iris.git ./vendor/src/github.com/kataras/iris
+gb vendor fetch
+
+#go get github.com/tools/godep
+
+#echo -e "\nDownloading Go packages:\n"
+#mkdir -p ./vendor/src/github.com/kataras/iris
+#git clone https://github.com/kataras/iris.git ./vendor/src/github.com/kataras/iris
