aa0b23cab819f08a5a639edd733c95989b4e7d3a
DJviolin <kerozin.joe@gmail.com>
2016-07-14 22:28:55 +0200

    refact

--- app/sockets.js
+++ app/sockets.js
@@ -4,10 +4,12 @@ module.exports = (io) => {
 
   io.on('connection', (socket) => {
     console.log('SERVER SAYS: A user connected');
+
     socket.emit('news', { hello: 'world from SERVER SIDE!' });
     socket.on('my other event', (data) => {
       console.log('SERVER SAYS: ', data);
     });
+
   });
 
 };
