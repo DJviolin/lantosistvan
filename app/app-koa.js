8c128ba7efe4dca1215efdb9156c4f4f2d4f7f81
DJviolin <kerozin.joe@gmail.com>
2016-07-16 20:03:54 +0200

    refact

--- app/app-koa.js
+++ app/app-koa.js
@@ -18,7 +18,7 @@ app.use(async function (ctx, next) {
   const start = new Date();
   await next();
   const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
 // response
--- app/start-koa.js
+++ app/start-koa.js
@@ -1,3 +1,5 @@
+// ab -n 1000 -c 100 http://127.0.0.1:3000/
+
 // start-koa.js
 /*require('babel-core').transform('code', {
   plugins: ['transform-async-to-generator']
