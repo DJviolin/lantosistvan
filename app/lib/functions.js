beca47dc93f2a061c7f85b56e074bdc101478619
DJviolin <kerozin.joe@gmail.com>
2016-07-16 16:00:51 +0200

    refact

--- app/lib/functions.js
+++ app/lib/functions.js
@@ -7,16 +7,27 @@ const config = require('../config/routes'),
 
 /////////////////////////////////////////////////////////////
 // READS DATABASE FROM JSON FILE
+// https://www.promisejs.org/
 /////////////////////////////////////////////////////////////
 
 //exports.fsAsync = function fsAsync(callback) {
 exports.fsAsync = (callback) => {
-
   fs.readFile(__dirname + db, 'utf8', (err, data) => {
     if (err) {
       throw callback(err);
     }
     callback(null, JSON.parse(data)); // null means no error, return results in callback
   });
+};
 
+exports.readJSON = (filename) => {
+  return new Promise((fulfill, reject) => {
+    fs.readFile(filename, 'utf8').done((data) => {
+      try {
+        fulfill(JSON.parse(data));
+      } catch (ex) {
+        reject(ex);
+      }
+    }, reject);
+  });
 };
