d808cbe2c83e4f7372c124542eb681ad67428234
DJviolin <kerozin.joe@gmail.com>
2016-07-17 00:04:43 +0200

    refact

--- app/koa/index.js
+++ app/koa/index.js
@@ -1,4 +1,8 @@
 // ab -n 1000 -c 100 http://127.0.0.1:3000/
 // koa2 koa-generator --hbs --git && cd koa-generator
+// > SET DEBUG=koa* & npm start koa-generator
+
+// https://github.com/koajs/koa/tree/v2.x#old-signature-middleware-v1x---deprecated
+
 require('babel-register');
 require('./app.js');
