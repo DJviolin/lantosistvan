f119301f42c68c93f5bc067e83960ee181503d1a
DJviolin <kerozin.joe@gmail.com>
2016-07-16 20:20:19 +0200

    refact

---
+++ app/koa/start.js
@@ -0,0 +1,3 @@
+// ab -n 1000 -c 100 http://127.0.0.1:3000/
+require('babel-register');
+require('./app.js');
--- app/start-koa.js
+++
@@ -1,9 +0,0 @@
-// ab -n 1000 -c 100 http://127.0.0.1:3000/
-
-// start-koa.js
-/*require('babel-core').transform('code', {
-  plugins: ['transform-async-to-generator']
-});*/
-require('babel-register');
-
-require('./app-koa.js');
