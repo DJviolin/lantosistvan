43b1fe5f8891b5152fb6147720e528b22c424a8d
DJviolin <kerozin.joe@gmail.com>
2016-07-17 20:11:10 +0200

    refact

--- app/app.js
+++ app/app.js
@@ -1,13 +1,5 @@
 //'use strict';
 
-// Performance
-// https://engineering.linkedin.com/nodejs/blazing-fast-nodejs-10-performance-tips-linkedin-mobile
-// https://strongloop.com/strongblog/node-js-performance-scaling-proxies-clusters/
-
-// async/await & try/catch
-// https://www.youtube.com/watch?v=COKdtOgopWQ
-// https://medium.com/@yamalight/danger-of-using-async-await-in-es7-8006e3eb7efb#.vsu0gxqdn
-
 /////////////////////////////////////////////////////////////
 // MODULE DEPENDENCIES
 /////////////////////////////////////////////////////////////
--- app/index.js
+++ app/index.js
@@ -4,6 +4,15 @@
 
 // ab -n 1000 -c 100 http://127.0.0.1:8081/
 
+// Performance
+// https://engineering.linkedin.com/nodejs/blazing-fast-nodejs-10-performance-tips-linkedin-mobile
+// https://strongloop.com/strongblog/node-js-performance-scaling-proxies-clusters/
+
+// async/await & try/catch
+// https://www.youtube.com/watch?v=COKdtOgopWQ
+// https://medium.com/@yamalight/danger-of-using-async-await-in-es7-8006e3eb7efb#.vsu0gxqdn
+// http://slides.com/stephenbelanger-1/easier-express-with-async-await#/1
+
 /*require('babel-core').transform('code', {
   presets: ['node6']
 });*/
