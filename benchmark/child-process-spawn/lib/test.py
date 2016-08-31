0f2282d333d231c9e2fe04b2fdee0b5efe8c6ee6
DJviolin <kerozin.joe@gmail.com>
2016-09-01 00:13:40 +0200

    refact

--- benchmark/child-process-spawn/lib/test.py
+++ benchmark/child-process-spawn/lib/test.py
@@ -2,20 +2,22 @@
 import sys
 #print(sys.version, ", ", end="")
 
+print(sys.argv, end="")
+
 first_arg = sys.argv[2]
 second_arg = sys.argv[3]
 filename = sys.argv[0]
 
 #data = first_arg + ", " + second_arg + "!"
 data = first_arg + ", " + second_arg
-print(data, end="")
+####print(data, end="")
 #sys.stdout.write(data)
 
 sys.stdout.flush()
 
 ####def main(argv):
-####    first_arg = argv[2]
-####    second_arg = argv[3]
+####    first_arg = sys.argv[2]
+####    second_arg = sys.argv[3]
 ####    data = first_arg + ", " + second_arg
 ####    print(data, end="")
 ####
