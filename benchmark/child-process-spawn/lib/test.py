f4517075f23f11c56db6f78d74c6c78c51a68408
DJviolin <kerozin.joe@gmail.com>
2016-08-31 20:11:34 +0200

    refact

--- benchmark/child-process-spawn/lib/test.py
+++ benchmark/child-process-spawn/lib/test.py
@@ -1,14 +1,25 @@
 # python_launched_from_nodejs.py
 import sys
+#print(sys.version, ", ", end="")
 
-print(sys.version, ", ", end="")
-
-first_arg = sys.argv[1]
-second_arg = sys.argv[2]
+first_arg = sys.argv[2]
+second_arg = sys.argv[3]
+filename = sys.argv[0]
 
 #data = first_arg + ", " + second_arg + "!"
-data = first_arg + ", " + second_arg + ", " + sys.argv[0]
+data = first_arg + ", " + second_arg
 print(data, end="")
 #sys.stdout.write(data)
 
 sys.stdout.flush()
+
+####def main(argv):
+####    first_arg = argv[2]
+####    second_arg = argv[3]
+####    data = first_arg + ", " + second_arg
+####    print(data, end="")
+####
+####if __name__ == "__main__":
+####    main(sys.argv[1:])
+####
+####sys.stdout.flush()
--- benchmark/child-process-spawn/test-objects.js
+++ benchmark/child-process-spawn/test-objects.js
@@ -3,16 +3,16 @@
 const path = require('path');
 const spawn = require('child_process').spawn;
 
-const exec = (file, argv1, argv2) => {
-  const py = spawn('python', [path.join(__dirname, file), argv1, argv2]);
+const exec = (file, fnCall, argv1, argv2) => {
+  const py = spawn('python', [path.join(__dirname, file), fnCall, argv1, argv2]);
   py.stdout.on('data', (chunk) => {
     const textChunk = chunk.toString('utf8'); // buffer to string
     const array = textChunk.split(', ');
     console.log(array);
   });
 };
-exec('lib/test.py', 'argument1', 'argument2'.length - 2);  // => [ 'argument1', '7' ]
-exec('lib/test.py', 'arg3', 'arg4'.length - 2);  // => [ 'arg3', '2' ]
+exec('lib/test.py', 'test', 'argument1', 'argument2'.length - 2);  // => [ 'argument1', '7' ]
+exec('lib/test.py', 'test', 'arg3', 'arg4'.length - 2);  // => [ 'arg3', '2' ]
 
 /*const execFile = require('child_process').execFile;
 const child = execFile('python', ['--version'], (error, stdout, stderr) => {
