efe91377a2580f95758db97f5989859618914919
DJviolin <kerozin.joe@gmail.com>
2016-08-31 16:41:17 +0200

    refact

--- benchmark/child-process-spawn/lib/test.py
+++ benchmark/child-process-spawn/lib/test.py
@@ -1,12 +1,14 @@
 # python_launched_from_nodejs.py
 import sys
+#from __future__ import print_function
 
 first_arg = sys.argv[1]
 second_arg = sys.argv[2]
 
 #data = first_arg + ", " + second_arg + "!"
-#print(data)
 data = first_arg + ", " + second_arg
-sys.stdout.write(data)
+#print(data)
+print(data, end="")
+#sys.stdout.write(data)
 
 sys.stdout.flush()
