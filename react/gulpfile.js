1734192bfed29fb3562f3680525698b36ffa6289
DJviolin <kerozin.joe@gmail.com>
2016-09-18 00:48:09 +0200

    refact

--- react/gulpfile.js
+++ react/gulpfile.js
@@ -6,8 +6,12 @@
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const webpack = require('webpack-stream');
+//const webpack = require('webpack-stream');
 //const webpackConfig = require('./webpack.config.js');
+
+const gutil = require('gulp-util');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
 
 const paths = {
   src: [
@@ -19,20 +23,56 @@ const paths = {
 // WEBPACK
 /////////////////////////////////////////////////////////////
 
-gulp.task('webpack', () =>
+/*gulp.task('webpack', () =>
   gulp.src('src/index.jsx')
     .pipe(babel({ presets: ['es2015', 'react'] })) // Also inserting Strict mode
     //.pipe(webpack(webpackConfig))
     .pipe(webpack({
+      debug: true,
       resolve: {
         extensions: ['', '.js', '.jsx'],
       },
+      entry: './src/index.jsx',
       output: {
         filename: 'bundle.js',
       },
     }))
     .pipe(gulp.dest('dist/'))
-);
+);*/
+
+/*gulp.task('webpack', (callback) => {
+  // run webpack
+  webpack({
+    // configuration
+  }, (err, stats) => {
+    if (err) throw new gutil.PluginError('webpack', err);
+    gutil.log('[webpack]', stats.toString({
+      // output options
+    }));
+    callback();
+  });
+});*/
+
+gulp.task('webpack', (callback) => {
+  // modify some webpack config options
+  const myConfig = Object.create(webpackConfig);
+  myConfig.plugins = myConfig.plugins.concat(
+    new webpack.DefinePlugin({
+      'process.env': {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify('production'),
+      },
+    })
+  );
+  // run webpack
+  webpack(myConfig, (err, stats) => {
+    if (err) throw new gutil.PluginError('webpack:build', err);
+    gutil.log('[webpack:build]', stats.toString({
+      colors: true,
+    }));
+    callback();
+  });
+});
 
 /////////////////////////////////////////////////////////////
 // INIT: APP
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -2,6 +2,11 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  debug: true,
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+  },
   // Important! Do not remove ''. If you do, imports without
   // an extension won't work anymore!
   resolve: {
