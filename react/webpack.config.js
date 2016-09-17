0548c8ae03cc22807124b558e6674c29d2e95e9e
DJviolin <kerozin.joe@gmail.com>
2016-09-17 23:08:48 +0200

    refact

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -2,8 +2,9 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  cache: true,
-  entry: {
-    react: './src/index',
+  // Important! Do not remove ''. If you do, imports without
+  // an extension won't work anymore!
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
   },
 };
