515d8339e16176467730400f9cfe66faa2b9d59e
DJviolin <kerozin.joe@gmail.com>
2016-09-18 16:43:03 +0200

    refact

--- react/src/js/styles.js
+++ react/src/js/styles.js
@@ -1,17 +1,23 @@
-const fonts = {
-  // serif
-  Georgia: 'Georgia, \'Times New Roman\', Times, serif',
-  Baskerville: 'Baskerville, \'Baskerville Old Face\', \'Hoefler Text\', Garamond, Times New Roman, serif',
-  Garamond: 'Garamond, Baskerville, \'Baskerville Old Face\', \'Hoefler Text\', Times New Roman, serif',
-  TimesNewRoman: 'TimesNewRoman, \'Times New Roman\', Times, Baskerville, Georgia, serif',
-  // sans-serif
-  Helvetica: '\'Helvetica Neue\', Helvetica, Arial, sans-serif',
-  HelveticaNeue: '\'Helvetica Neue\', Helvetica, Arial, sans-serif',
-  HelveticaNeueLight: '\'HelveticaNeue-Light\', \'Helvetica Neue Light\', \'Helvetica Neue\', Helvetica, Arial, \'Lucida Grande\', sans-serif',
-  TrebuchetMS: '\'Trebuchet MS\', \'Lucida Sans Unicode\', \'Lucida Sans\', Tahoma, sans-serif',
-  Verdana: 'Verdana, Geneva, sans-serif',
-  GillSans: '\'Gill Sans\', \'Gill Sans MT\', Calibri, sans-serif',
-  Futura: 'Futura, \'Trebuchet MS\', Arial, sans-serif',
+const variables = {
+  fonts: {
+    // serif
+    Georgia: 'Georgia, \'Times New Roman\', Times, serif',
+    Baskerville: 'Baskerville, \'Baskerville Old Face\', \'Hoefler Text\', Garamond, Times New Roman, serif',
+    Garamond: 'Garamond, Baskerville, \'Baskerville Old Face\', \'Hoefler Text\', Times New Roman, serif',
+    TimesNewRoman: 'TimesNewRoman, \'Times New Roman\', Times, Baskerville, Georgia, serif',
+    // sans-serif
+    Helvetica: '\'Helvetica Neue\', Helvetica, Arial, sans-serif',
+    HelveticaNeue: '\'Helvetica Neue\', Helvetica, Arial, sans-serif',
+    HelveticaNeueLight: '\'HelveticaNeue-Light\', \'Helvetica Neue Light\', \'Helvetica Neue\', Helvetica, Arial, \'Lucida Grande\', sans-serif',
+    TrebuchetMS: '\'Trebuchet MS\', \'Lucida Sans Unicode\', \'Lucida Sans\', Tahoma, sans-serif',
+    Verdana: 'Verdana, Geneva, sans-serif',
+    GillSans: '\'Gill Sans\', \'Gill Sans MT\', Calibri, sans-serif',
+    Futura: 'Futura, \'Trebuchet MS\', Arial, sans-serif',
+  },
+  fontStyles: {
+    fontSize: '16px',
+    lineHeight: '24px',
+  },
 };
 
 const styles = {
@@ -19,14 +25,12 @@ const styles = {
     Georgia: 'Georgia, \'Times New Roman\', Times, serif',
   },*/
   //Styles
-  body: {
-    margin: 0,
-    padding: 0,
-  },
   App: {
     textAlign: 'center',
-    fontFamily: fonts.Georgia,
+    fontFamily: variables.fonts.Georgia,
     //get fontFamily() { return styles.fonts.Georgia; },
+    fontSize: variables.fontStyles.fontSize,
+    lineHeight: variables.fontStyles.lineHeight,
   },
   AppLogo: {
     animation: 'App-logo-spin infinite 20s linear',
