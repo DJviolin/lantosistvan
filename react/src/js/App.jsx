d8139bb159caa0434c35415aaa2d37f4cb774dc8
DJviolin <kerozin.joe@gmail.com>
2016-09-18 19:52:32 +0200

    refact

--- react/src/js/App.jsx
+++ react/src/js/App.jsx
@@ -21,19 +21,19 @@ import styles from './styles.js';
   },
 }*/
 
-const App = () => {
-  const user = 'Lanti';
-  return (
-    <div className="App" style={styles.container}>
-      <div style={styles.header}>
-        <img src="./dist/images/logo.svg" alt="logo" style={styles.logo} />
-        <h2>Welcome to React, {user}</h2>
-      </div>
-      <p style={styles.intro}>
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+const App = ({ name }) => (
+  <div className="App" style={styles.container}>
+    <div style={styles.header}>
+      <img src="./dist/images/logo.svg" alt="logo" style={styles.logo} />
+      <h2>Welcome to React, {`${name}!`}</h2>
     </div>
-  );
-};
+    <p style={styles.intro}>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
+
+App.propTypes = { name: React.PropTypes.string.isRequired };
+App.defaultProps = { name: 'Lanti' };
 
 export default App;
