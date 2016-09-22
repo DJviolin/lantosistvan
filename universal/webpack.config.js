7a94a00711e9584195618fab35c9ee1ce81103e4
DJviolin <kerozin.joe@gmail.com>
2016-09-22 18:14:32 +0200

    refact

--- universal/webpack.config.js
+++ universal/webpack.config.js
@@ -53,16 +53,6 @@ module.exports = {
         test: /\.json$/,
         loader: 'json',
       },
-      // "file" loader makes sure those assets end up in the `build` folder.
-      // When you `import` an asset, you get its filename.
-      {
-        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-        exclude: /\/favicon.ico$/,
-        loader: 'file',
-        query: {
-          name: 'images/webpack/[name].[hash:8].[ext]',
-        },
-      },
     ],
   },
   //plugins: debug ? [] : [
