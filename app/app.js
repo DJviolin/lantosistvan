a52096f91a7bfcb21300c25de83093afcfd2283f
DJviolin <kerozin.joe@gmail.com>
2016-09-23 12:25:04 +0200

    refact

--- app/app.js
+++ app/app.js
@@ -40,6 +40,18 @@ const tube = require('./routes/tube');
 const app = express();
 //app.set('strict routing', false);
 
+// Set headers (before routes)
+// http://enable-cors.org/server_expressjs.html
+app.use((req, res, next) => {
+  //res.set('Access-Control-Allow-Origin', '*');
+  //res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+  });
+  next();
+});
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // CUSTOM CONSOLE
 // https://nodejs.org/api/console.html
