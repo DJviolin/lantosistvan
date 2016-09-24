0521864066eb34346959c3efce985c9605c14ce7
DJviolin <kerozin.joe@gmail.com>
2016-09-24 12:35:39 +0200

    refact

--- app/routes/user.js
+++ app/routes/user.js
@@ -40,19 +40,6 @@ router.get('/setup', (req, res) => {
   });
 });
 
-// TODO: route middleware to verify a token
-// route to show a random message (GET http://127.0.0.1:8081/user/)
-router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the coolest API on earth!' });
-});
-// route to return all users (GET http://127.0.0.1:8081/user/users)
-router.get('/users', (req, res) => {
-  //console.log(req.app.get('superSecret'));
-  User.find({}, (err, users) => {
-    res.json(users);
-  });
-});
-
 // route to authenticate a user (POST http://127.0.0.1:8081/user/authenticate)
 router.post('/authenticate', (req, res) => {
   // find the user
@@ -81,6 +68,71 @@ router.post('/authenticate', (req, res) => {
         });
       }
     }
+  });
+});
+
+/*// route middleware to verify a token
+router.use((req, res, next) => {
+  // check header or url parameters or post parameters for token
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, req.app.get('jwtTokenSecret'), (err, decoded) => {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      }
+      // if everything is good, save to request for use in other routes
+      req.decoded = decoded;
+      console.log(req.decoded);
+      return next();
+    });
+  }
+  // if there is no token
+  // return an error
+  return res.status(403).send({
+    success: false,
+    message: 'No token provided.',
+  });
+});*/
+
+// ---------------------------------------------------------
+// route middleware to authenticate and check token
+// ---------------------------------------------------------
+router.use((req, res, next) => {
+  // check header or url parameters or post parameters for token
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, req.app.get('jwtTokenSecret'), (err, decoded) => {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.',
+    });
+  }
+});
+
+// route to show a random message (GET http://127.0.0.1:8081/user/)
+router.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+// route to return all users (GET http://127.0.0.1:8081/user/users)
+router.get('/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) throw err;
+    return res.json(users);
   });
 });
 
