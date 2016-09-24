8b03a53ea02a45eaaea30d5316e43e696d341b52
DJviolin <kerozin.joe@gmail.com>
2016-09-24 19:04:59 +0200

    refact

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,25 +5,6 @@ const router = express.Router();
 const functions = require('../lib/functions');
 const fsAsync = functions.fsAsync;
 
-router.route('/test')
-  .get((req, res) => {
-    const html =
-      '<form action="" method="POST">\n\
-         Enter your name:\n\
-         <input type="text" name="userName" placeholder="..." />\n\
-         <br>\n\
-         <button type="submit">Submit</button>\n\
-      </form>';
-    res.send(html);
-  })
-  .post((req, res) => {
-    const userName = req.body.userName;
-    const html =
-      `Hello: ${userName}.<br>\n\
-      <a href="/hu/test">Try again.</a>`;
-    res.send(html);
-  });
-
 /////////////////////////////////////////////////////////////
 // INTERNAL API
 // RETURNS JOURNAL FRONTPAGE
