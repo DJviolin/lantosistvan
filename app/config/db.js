0e15791b57f9c00c4a9ebfea0b6a33676df79816
DJviolin <kerozin.joe@gmail.com>
2016-09-24 10:16:55 +0200

    refact

--- app/config/db.js
+++ app/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  secret: 'password', // used when we create and verify JSON Web Tokens
+  secret: 'ilovescotchyscotch', // used when we create and verify JSON Web Tokens
   database: 'mongodb://127.0.0.1:27017/test', // the URI with username and password to your MongoDB installation, database at the end
 };
--- app/routes/user.js
+++ app/routes/user.js
@@ -7,9 +7,9 @@ const router = express.Router();
 // Json Web Token Test
 /////////////////////////////////////////////////////////////
 
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
   res.send('/api-test route works!');
-});
+});*/
 
 /*router.route('/all')
   .get((req, res) => {
@@ -30,13 +30,24 @@ router.get('/setup', (req, res) => {
     password: 'password',
     admin: true,
   });
-
   // save the sample user
   nick.save((err) => {
     if (err) throw err;
-
     console.log('User saved successfully');
     res.json({ success: true });
+  });
+});
+
+// TODO: route to authenticate a user (POST http://127.0.0.1:8081/user/authenticate)
+// TODO: route middleware to verify a token
+// route to show a random message (GET http://127.0.0.1:8081/user/)
+router.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+// route to return all users (GET http://127.0.0.1:8081/user/users)
+router.get('/users', (req, res) => {
+  User.find({}, (err, users) => {
+    res.json(users);
   });
 });
 
