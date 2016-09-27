b00be66d0a336e7a4edd7deccacf22ed5fd23447
DJviolin <kerozin.joe@gmail.com>
2016-09-27 10:23:48 +0200

    refact

--- app/sql/config/pgQuery.js
+++ app/sql/config/pgQuery.js
@@ -16,6 +16,7 @@ const pool = require('./pgConfig');
 };*/
 
 // https://github.com/brianc/node-postgres/wiki/Parameterized-queries-and-Prepared-Statements
+// https://github.com/felixfbecker/node-sql-template-strings
 module.exports = function SQL(parts, ...values) {
   return {
     text: parts.reduce((prev, curr, i) => prev + '$' + i + curr),
